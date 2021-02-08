b6f07c911747c7669f34d199607d42b5d7f2d9d8
SubvertDev <subvert.dev@gmail.com>
2021-02-08 18:49:40 +0300

    Add textField to AddItem controller and some handling for that

--- Checklists/AddItemTableViewController.swift
+++ Checklists/AddItemTableViewController.swift
@@ -7,23 +7,55 @@
 
 import UIKit
 
-class AddItemTableViewController: UITableViewController {
+class AddItemTableViewController: UITableViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         navigationItem.largeTitleDisplayMode = .never
-
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
 
+    //MARK: - TableView Delegate
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
     //MARK: - Actions
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
         navigationController?.popViewController(animated: true)
     }
     
-    @IBAction func done(_ sender: UIBarButtonItem) {
+    @IBAction func done() {
+        
+        print("Text field value:\(textField.text!)")
+        
         navigationController?.popViewController(animated: true)
+        
+    }
+    
+    //MARK: - Text Field Delegate
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in:stringRange, with: string)
+        doneButton.isEnabled = !newText.isEmpty
+        
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneButton.isEnabled = false
+        return true
     }
     
 }
