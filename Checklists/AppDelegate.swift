0cb97249ceb287c629861f23084a274297b9d45c
SubvertDev <subvert.dev@gmail.com>
2021-02-23 20:15:04 +0300

    Move permission ask from startup to actual remind switch

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -37,7 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     // MARK: - User Notification Delegates
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        print("Received local notification \(notification)")
+        //print("Received local notification \(notification)")
     }
 }
 
